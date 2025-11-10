2d295c2629fb856feeea25778c7feb21fabc955a
James Monroe <jay@jamesmonroedesign.com>
2025-11-09 21:37:09 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // IMPORTANT: Replace 'your-repo-name' with the actual name of your GitHub repository
-  base: '/your-repo-name/', 
+  base: '/therapist-finder/', 
   plugins: [react()],
 })
